83453005ea0f631d108b2b467d6518d2f187df03
Fahad Murtaza <fahdim@gmail.com>
2020-05-03 04:30:22 +0400

    Code formatting

--- core/ACFCTR_Core.php
+++ core/ACFCTR_Core.php
@@ -4,24 +4,21 @@
  * ACF Conditional Taxonomy Rules Core
  * =======================================
  *
- * 
+ *
  * @author Matt Keys <matt@mattkeys.me>
  */
 
-class ACFCTR_Core
-{
+class ACFCTR_Core {
 
-	public function init()
-	{
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_script' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_script' ) );
+	public function init() {
+		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_script' ] );
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_script' ] );
 	}
 
-	public function enqueue_script()
-	{
-		wp_enqueue_script( 'acf-input-conditional-taxonomy', ACFCTR_PUBLIC_PATH . 'includes/input_conditional_taxonomy.js', array( 'acf-input' ), '3.0.0' );
+	public function enqueue_script() {
+		wp_enqueue_script( 'acf-input-conditional-taxonomy', ACFCTR_PUBLIC_PATH . 'includes/input_conditional_taxonomy.js', [ 'acf-input' ], '3.0.0' );
 	}
 
 }
 
-add_action( 'plugins_loaded', array( new ACFCTR_Core, 'init' ) );
+add_action( 'plugins_loaded', [ new ACFCTR_Core, 'init' ] );
--- includes/input_conditional_taxonomy.js
+++ includes/input_conditional_taxonomy.js
@@ -1,56 +1,57 @@
-(function($){
+(function($) {
 
-	// a few helper functions copied from acf-input.js
-	var parseString = function( val ){
-		return val ? '' + val : '';
-	};
-	var isEqualTo = function( v1, v2 ){
-		return ( parseString(v1).toLowerCase() === parseString(v2).toLowerCase() );
-	};
-	var inArray = function( v1, array ){
+  // a few helper functions copied from acf-input.js
+  var parseString = function(val) {
+    return val ? '' + val : '';
+  };
+  var isEqualTo = function(v1, v2) {
+    return (parseString(v1).toLowerCase() === parseString(v2).toLowerCase());
+  };
+  var inArray = function(v1, array) {
 
-		// cast all values as string
-		array = array.map(function(v2){
-			return parseString(v2);
-		});
+    // cast all values as string
+    array = array.map(function(v2) {
+      return parseString(v2);
+    });
 
-		return (array.indexOf( v1 ) > -1);
-	}
+    return (array.indexOf(v1) > -1);
+  };
 
-	// add our new conditional for matching against taxonomy IDs
-	var TaxonomyEqualTo = acf.Condition.extend({
-		type: 'taxonomyEqualTo',
-		operator: '==',
-		label: 'Selection Term ID equals',
-		fieldTypes: [ 'taxonomy' ],
-		match: function( rule, field ){
-			var val = field.val();
-			if ( val instanceof Array ) {
-				return inArray( rule.value, val );
-			} else {
-				return isEqualTo( rule.value, val );
-			}
-		},
-		choices: function( fieldObject ){
-			return '<input type="number" />';
-		}
-	});
+  // add our new conditional for matching against taxonomy IDs
+  var TaxonomyEqualTo = acf.Condition.extend({
+    type: 'taxonomyEqualTo',
+    operator: '==',
+    label: 'Selection Term ID equals',
+    fieldTypes: ['taxonomy'],
+    match: function(rule, field) {
+      var val = field.val();
+      if (val instanceof Array) {
+        return inArray(rule.value, val);
+      }
+      else {
+        return isEqualTo(rule.value, val);
+      }
+    },
+    choices: function(fieldObject) {
+      return '<input type="number" />';
+    },
+  });
 
-	acf.registerConditionType( TaxonomyEqualTo );
+  acf.registerConditionType(TaxonomyEqualTo);
 
-	var TaxonomyNotEqualTo = TaxonomyEqualTo.extend({
-		type: 'taxonomyNotEqualTo',
-		operator: '!=',
-		label: "Selection Term ID not equal to",
-		fieldTypes: ['taxonomy'],
-		match: function (rule, field) {
-			return !TaxonomyEqualTo.prototype.match.apply(this, arguments)
-		},
-		choices: function (fieldObject) {
-			return '<input type="number" />';
-		}
-	});
+  var TaxonomyNotEqualTo = TaxonomyEqualTo.extend({
+    type: 'taxonomyNotEqualTo',
+    operator: '!=',
+    label: 'Selection Term ID not equal to',
+    fieldTypes: ['taxonomy'],
+    match: function(rule, field) {
+      return !TaxonomyEqualTo.prototype.match.apply(this, arguments);
+    },
+    choices: function(fieldObject) {
+      return '<input type="number" />';
+    },
+  });
 
-	acf.registerConditionType(TaxonomyNotEqualTo);
+  acf.registerConditionType(TaxonomyNotEqualTo);
 
 })(jQuery);
